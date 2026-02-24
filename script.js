5c35d182a9340911e2f22d6ed7b22bd8e4b86657
afazuddinabid-stack <afazuddinabid@gmail.com>
2026-02-24 21:19:24 +0600

    created event deligation

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ let interview=document.getElementById("interview-count");
 let rejected=document.getElementById("rejected-count");
 
 
+const allfilterbtn=document.getElementById("all-filter-btn");
+const interviewfilterbtn=document.getElementById("interview-filter-btn");
+const rejectedfilterbtn=document.getElementById("rejected-filter-btn");
+
 const allcards=document.getElementById("all-cards");
 
 const maincontainer=document.querySelector("main");
@@ -17,4 +21,33 @@ function calculatecount(){
 calculatecount();
 
 function Togglestyle(id){
-    console.log("click",id)}
+    allfilterbtn.classList.remove("btn-primary");
+    interviewfilterbtn.classList.remove("btn-primary");
+    rejectedfilterbtn.classList.remove("btn-primary");
+
+    allfilterbtn.classList.add("bg-gray-300");
+    interviewfilterbtn.classList.add("bg-gray-300");
+    rejectedfilterbtn.classList.add("bg-gray-300");
+    const selected=document.getElementById(id);
+    selected.classList.remove("bg-gray-300");
+    selected.classList.add("btn-primary");
+    
+}
+
+maincontainer.addEventListener('click', function (event) {
+    const parentNode = event.target.parentNode.parentNode;
+    const companyname =parentNode.querySelector(".company-name").innerText;
+    const positionname =parentNode.querySelector(".position-name").innerText;
+    const salary =parentNode.querySelector(".salary").innerText;
+    const status =parentNode.querySelector(".status").innerText;
+    const discription=parentNode.querySelector(".discription").innerText;
+
+    const carddetails={
+        companyname,
+        positionname,
+        salary,
+        status,
+        discription
+    }
+
+})
